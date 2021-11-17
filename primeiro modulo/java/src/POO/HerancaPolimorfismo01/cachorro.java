d8797288d84f8c38833913cf22557fe59bb2cb7d
Rafael Borges <rafaelborges.av@gmail.com>
2021-11-17 16:15:49 -0300

    versao 5

---
+++ primeiro modulo/java/src/POO/HerancaPolimorfismo01/cachorro.java
@@ -0,0 +1,13 @@
+package POO.HerancaPolimorfismo01;
+
+public class cachorro extends AnimalMain {
+	
+	public void emitirSom() {
+		System.out.println("auuuuuuuuuuu au");	
+	}
+	
+	public void correr() {
+		System.out.println("correndo..");
+	}
+
+}
